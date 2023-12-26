96b72acba2f3e20dd18730e76e376ae92da489e1
Corentin Tournier <corentin@MacBook-Pro-de-Corentin.local>
2023-12-26 21:39:24 +0100

    Variable CSS height

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import styles from './page.module.css'
 import { Canvas } from '@react-three/fiber'
 import Iphone from './components/Iphone';
@@ -11,36 +11,31 @@ import { ResponsiveProvider, useResponsive } from './contexts/Responsive3D';
 
 export default function Home() {
 
-  const popupResponsiveHeight = {
-    height: typeof window !== 'undefined' && window.innerHeight
-  }
-
-  const getPopupStyle = () => {
-    if (popupResponsiveHeight.height !== false) {
-      return { height: popupResponsiveHeight.height };
+  useLayoutEffect(() => {
+    const popupResponsiveHeight2 = {
+      height: typeof window !== 'undefined' && window.innerHeight
     }
-    return {}; // Retourne un objet vide si la hauteur est false
-  };
+
+    console.log(popupResponsiveHeight2.height)
+
+    document.documentElement.style.setProperty('--fullheight', `${popupResponsiveHeight2.height}px`);
+  })
 
   interface PopupProps {
     isOpen: boolean;
     children: React.ReactNode;
-    popupResponsiveHeight: {
-      height: number | false;
-    };
   }  
 
-  const Popup = ({ isOpen, children, popupResponsiveHeight }: PopupProps) => {
+  const Popup = ({ isOpen, children }: PopupProps) => {
   
     return isOpen ? (
-      <div id={styles.popupoverlay} style={getPopupStyle()}>
+      <div id={styles.popupoverlay}>
         <div id={styles.popup}>
           {children}
         </div>
       </div>
     ) : null;
   };
-  
 
   const [isPopupOpen, setPopupOpen] = useState(false);
 
@@ -56,8 +51,8 @@ export default function Home() {
     <ResponsiveProvider>
     <CustomizationProvider>
     <main className={styles.main}>
-      <section id={styles.herosection} style={getPopupStyle()}>
-        <h1>iPhone 15 Pro</h1>
+      <section id={styles.herosection}>
+          <h1>iPhone 15 Pro</h1>
       </section>
       <section id={styles.canvas}>
         <Canvas>
@@ -73,7 +68,7 @@ export default function Home() {
       <button id={styles.customizationbutton} onClick={openPopup}>
         <svg height="50px" id="Layer_1" version="1.1" viewBox="0 0 50 50" width="50px" xmlns="http://www.w3.org/2000/svg"><path d="M8,14L4,49h42l-4-35H8z" fill="none" stroke="#ffffff"/><rect fill="none" height="50" width="50"/><path d="M34,19c0-1.241,0-6.759,0-8  c0-4.971-4.029-9-9-9s-9,4.029-9,9c0,1.241,0,6.759,0,8" fill="none" stroke="#ffffff" /><circle cx="34" cy="19" r="2"/><circle cx="16" cy="19" r="2"/></svg>
       </button>
-      <Popup isOpen={isPopupOpen} popupResponsiveHeight={popupResponsiveHeight}>
+      <Popup isOpen={isPopupOpen}>
           <Canvas>
             <Experience/>
             <directionalLight position={[0, 0, -1]} intensity={2}/>
